9b309dce8bf67a712f052b71c9e00004dc2f1396
vinisooo <cviniciusliramdev@gmail.com>
2023-03-01 20:07:01 -0400

    feat: adds user route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application } from "express";
+import { userRouter } from "./routers/users.routes";
 
 const app: Application = express();
 app.use(express.json());
+
+app.use("/users",userRouter);
 
 export default app
---
+++ src/routers/users.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+export const userRouter: Router =  Router();
+
+userRouter.post("/");
