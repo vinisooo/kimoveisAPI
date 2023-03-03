d9ce6825ef540d180d647137638eeeb5a6a662af
vinisooo <cviniciusliramdev@gmail.com>
2023-03-02 20:46:31 -0400

    feat: adds category router

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import "express-async-errors";
 import { loginRouter } from './routers/login.routes';
-import express, { Application } from "express";
 import { userRouter } from "./routers/users.routes";
+import { categoriesRouter } from "./routers/categories.routes";
+import express, { Application } from "express";
 import { handleError } from "./errors";
 
 const app: Application = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/login", loginRouter);
+app.use("/categories", categoriesRouter);
 
 app.use(handleError);
 
---
+++ src/routers/categories.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+export const categoriesRouter: Router = Router();
+
+categoriesRouter.post("/");
