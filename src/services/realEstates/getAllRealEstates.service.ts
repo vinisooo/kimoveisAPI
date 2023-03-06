61ad26bf0c6a0c59f4a481e467171351b840f478
vinisooo <cviniciusliramdev@gmail.com>
2023-03-05 21:51:53 -0400

    fix: renames variable

--- src/services/realEstates/getAllRealEstates.service.ts
+++ src/services/realEstates/getAllRealEstates.service.ts
@@ -6,10 +6,10 @@ import { AppDataSource } from './../../data-source';
 export const getAllRealEstatesService = async() => {
     const realEstatesRepo: Repository<RealEstate> = AppDataSource.getRepository(RealEstate);
 
-    const allRealEstatesRepo = await realEstatesRepo
+    const allRealEstates = await realEstatesRepo
     .createQueryBuilder("estates")
     .leftJoinAndSelect("estates.address", "address")
     .getMany();
 
-    return allRealEstatesRepo;
+    return allRealEstates;
 }
